433eb06aaf86f959db51be30bab42dfd22d21d84
loc.tv <loc1852002@gmail.com>
2025-11-26 15:33:29 +0700

    fix dinamic library

--- code/pilotnet_train/model_pilotnet.py
+++ code/pilotnet_train/model_pilotnet.py
@@ -1,24 +1,21 @@
 # model_pilotnet_tf.py
 
 import tensorflow as tf
-from tensorflow.keras.layers import *
-from tensorflow.keras.models import Model
-
+from tensorflow import keras  # type: ignore
 
 def PilotNetSmall(input_shape=(120, 160, 1)):
-    inputs = Input(shape=input_shape)
+    inputs = keras.Input(shape=input_shape)
 
-    x = Conv2D(24, 5, strides=2, activation='relu')(inputs)
-    x = Conv2D(36, 5, strides=2, activation='relu')(x)
-    x = Conv2D(48, 5, strides=2, activation='relu')(x)
-    x = Conv2D(64, 3, strides=1, activation='relu')(x)
-    x = Conv2D(64, 3, strides=1, activation='relu')(x)
+    x = keras.layers.Conv2D(24, 5, strides=2, activation='relu')(inputs)
+    x = keras.layers.Conv2D(36, 5, strides=2, activation='relu')(x)
+    x = keras.layers.Conv2D(48, 5, strides=2, activation='relu')(x)
+    x = keras.layers.Conv2D(64, 3, strides=1, activation='relu')(x)
+    x = keras.layers.Conv2D(64, 3, strides=1, activation='relu')(x)
+    x = keras.layers.Flatten()(x)
 
-    x = Flatten()(x)
+    x = keras.layers.Dense(100, activation='relu')(x)
+    x = keras.layers.Dense(50, activation='relu')(x)
+    x = keras.layers.Dense(10, activation='relu')(x)
+    outputs = keras.layers.Dense(1)(x)
 
-    x = Dense(100, activation='relu')(x)
-    x = Dense(50, activation='relu')(x)
-    x = Dense(10, activation='relu')(x)
-    outputs = Dense(1)(x)
-
-    return Model(inputs, outputs)
+    return keras.Model(inputs, outputs)
